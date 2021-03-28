cacef0f15f33a3250454538d5634c5f09726352e
Tiger J. Wang <tiger.wangj@gmail.com>
2021-03-28 17:39:08 -0400

    Initial Commit

---
+++ Yalies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yalies
+//
+//  Created by Tiger J. Wang on 3/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
